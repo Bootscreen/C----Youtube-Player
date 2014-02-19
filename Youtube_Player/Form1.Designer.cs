2de560d92084095bfc7d139c7835ddc2e0990ff0
Bootscreen <bootscreen88@gmail.com>
2014-02-19 13:09:30 +0100

    first

---
+++ Youtube_Player/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Youtube_Player
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.YTplayer = new AxShockwaveFlashObjects.AxShockwaveFlash();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.YTplayer)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox1.ForeColor = System.Drawing.Color.White;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(0, 435);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(714, 128);
+			this.listBox1.TabIndex = 1;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.ForeColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(0, 566);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(485, 26);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(483, 566);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(116, 26);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// YTplayer
+			// 
+			this.YTplayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.YTplayer.Enabled = true;
+			this.YTplayer.Location = new System.Drawing.Point(0, 0);
+			this.YTplayer.Name = "YTplayer";
+			this.YTplayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("YTplayer.OcxState")));
+			this.YTplayer.Size = new System.Drawing.Size(714, 434);
+			this.YTplayer.TabIndex = 4;
+			this.YTplayer.FlashCall += new AxShockwaveFlashObjects._IShockwaveFlashEvents_FlashCallEventHandler(this.YTplayer_FlashCall);
+			this.YTplayer.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.YTplayer_ControlAdded);
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Enabled = false;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(598, 566);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(116, 26);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Remove";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ClientSize = new System.Drawing.Size(714, 592);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.YTplayer);
+			this.MinimumSize = new System.Drawing.Size(730, 630);
+			this.Name = "Form1";
+			this.Text = "Youtube Player";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.YTplayer)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private AxShockwaveFlashObjects.AxShockwaveFlash YTplayer;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ Youtube_Player/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using WMPLib;
+
+namespace Youtube_Player
+{
+	public partial class Form1 : Form
+	{
+		int listbox_play_index = -1;
+		int width = 0;
+		int height = 0;
+		string url_global = "";
+		bool ende = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string url = textBox1.Text;
+
+			if (url.Length > 0)
+			{
+				url = url.Replace("watch?v=", "v/");
+				if (url.Contains('&'))
+				{
+					url = url.Remove(url.IndexOf('&'));
+				}
+				listBox1.Items.Add(url);
+
+				textBox1.Text = "";
+			}
+		}
+
+		private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			int index = this.listBox1.IndexFromPoint(e.Location);
+			if (index != System.Windows.Forms.ListBox.NoMatches)
+			{
+				if(listbox_play_index != index)
+				{
+					if (listbox_play_index >= 0)
+					{
+						if(listBox1.Items[listbox_play_index].ToString().StartsWith("»") || listBox1.Items[listbox_play_index].ToString().StartsWith("ǁ"))
+						{
+							listBox1.Items[listbox_play_index] = listBox1.Items[listbox_play_index].ToString().Substring(2);
+						}
+					}
+					if (listBox1.Items[index].ToString().StartsWith("»") || listBox1.Items[index].ToString().StartsWith("ǁ"))
+					{
+						listBox1.Items[index] = listBox1.Items[index].ToString().Substring(2);
+					}
+
+					string item = listBox1.Items[index].ToString();
+					item = item + "?autoplay=1&version=3&enablejsapi=1&hd=1";
+
+					listbox_play_index = index;
+
+					YTplayer.Movie = item;
+					YTplayer.Play();
+				}
+			}
+		}
+
+		private void YTplayer_FlashCall(object sender, AxShockwaveFlashObjects._IShockwaveFlashEvents_FlashCallEvent e)
+		{
+			try
+			{
+				if (e.request.Contains("onYouTubePlayerReady") ||
+					e.request.Contains("YTStateChange") ||
+					e.request.Contains("YTError"))
+				{
+					Console.Write("YTplayer_FlashCall: raw: " + e.request.ToString() + "\r\n");
+					// message is in xml format so we need to parse it
+					XmlDocument document = new XmlDocument();
+					document.LoadXml(e.request);
+					// get attributes to see which command flash is trying to call
+					XmlAttributeCollection attributes = document.FirstChild.Attributes;
+					String command = attributes.Item(0).InnerText;
+					// get parameters
+					XmlNodeList list = document.GetElementsByTagName("arguments");
+					List<string> listS = new List<string>();
+					foreach (XmlNode l in list)
+					{
+						listS.Add(l.InnerText);
+					}
+					Console.Write("YTplayer_FlashCall: \"" + command.ToString() + "(" + string.Join(",", listS) + ")\r\n");
+					// Interpret command
+					switch (command)
+					{
+						case "onYouTubePlayerReady": YTready(listS[0]); break;
+						case "YTStateChange": YTStateChange(listS[0]); break;
+						case "YTError": YTStateError(listS[0]); break;
+						default: Console.Write("YTplayer_FlashCall: (unknownCommand: " + command + ")\r\n"); break;
+					}
+				}
+				else
+				{
+					e.request = "";
+				}
+				/*if (e.request.Contains("ads"))
+				{
+					Console.Write("YTplayer_FlashCall: raw: " + e.request.ToString() + "\r\n");
+				}*/
+			}
+			catch(Exception ex)
+			{
+			}
+		}
+
+		private string YTplayer_CallFlash(string ytFunction)
+		{
+			string flashXMLrequest = "";
+			string response = "";
+			string flashFunction = "";
+			List<string> flashFunctionArgs = new List<string>();
+
+			Regex func2xml = new Regex(@"([a-z][a-z0-9]*)(\(([^)]*)\))?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+			Match fmatch = func2xml.Match(ytFunction);
+
+			if (fmatch.Captures.Count != 1)
+			{
+				Console.Write("bad function request string");
+				return "";
+			}
+
+			flashFunction = fmatch.Groups[1].Value.ToString();
+			flashXMLrequest = "<invoke name=\"" + flashFunction + "\" returntype=\"xml\">";
+			if (fmatch.Groups[3].Value.Length > 0)
+			{
+				flashFunctionArgs = parseDelimitedString(fmatch.Groups[3].Value);
+				if (flashFunctionArgs.Count > 0)
+				{
+					flashXMLrequest += "<arguments><string>";
+					flashXMLrequest += string.Join("</string><string>", flashFunctionArgs);
+					flashXMLrequest += "</string></arguments>";
+				}
+			}
+			flashXMLrequest += "</invoke>";
+
+			try
+			{
+				Console.Write("YTplayer_CallFlash: \"" + flashXMLrequest + "\"\r\n");
+				response = YTplayer.CallFunction(flashXMLrequest);
+				Console.Write("YTplayer_CallFlash_response: \"" + response + "\"\r\n");
+			}
+			catch
+			{
+				Console.Write("YTplayer_CallFlash: error \"" + flashXMLrequest + "\"\r\n");
+			}
+
+			return response;
+		}
+
+		private static List<string> parseDelimitedString(string arguments, char delim = ',')
+		{
+			bool inQuotes = false;
+			bool inNonQuotes = false;
+			int whiteSpaceCount = 0;
+
+			List<string> strings = new List<string>();
+
+			StringBuilder sb = new StringBuilder();
+			foreach (char c in arguments)
+			{
+				if (c == '\'' || c == '"')
+				{
+					if (!inQuotes)
+						inQuotes = true;
+					else
+						inQuotes = false;
+
+					whiteSpaceCount = 0;
+				}
+				else if (c == delim)
+				{
+					if (!inQuotes)
+					{
+						if (whiteSpaceCount > 0 && inQuotes)
+						{
+							sb.Remove(sb.Length - whiteSpaceCount, whiteSpaceCount);
+							inNonQuotes = false;
+						}
+						strings.Add(sb.Replace("'", string.Empty).Replace("\"", string.Empty).ToString());
+						sb.Remove(0, sb.Length);
+					}
+					else
+					{
+						sb.Append(c);
+					}
+					whiteSpaceCount = 0;
+				}
+				else if (char.IsWhiteSpace(c))
+				{
+					if (inNonQuotes || inQuotes)
+					{
+						sb.Append(c);
+						whiteSpaceCount++;
+					}
+				}
+				else
+				{
+					if (!inQuotes) inNonQuotes = true;
+					sb.Append(c);
+					whiteSpaceCount = 0;
+				}
+			}
+			strings.Add(sb.Replace("'", string.Empty).Replace("\"", string.Empty).ToString());
+
+
+			return strings;
+		}
+
+		private void YTready(string playerID)
+		{
+			//YTState = true;
+			//start eventHandlers
+			YTplayer_CallFlash("addEventListener(\"onStateChange\",\"YTStateChange\")");
+			YTplayer_CallFlash("addEventListener(\"onError\",\"YTError\")");
+			YTplayer_CallFlash("setPlaybackQuality(\"hd720\")");
+		}
+
+		private void YTStateChange(string YTplayState)
+		{
+			switch (int.Parse(YTplayState))
+			{
+				case -1:	//not started yet
+					break;
+				case 1:		//playing
+					if (listBox1.Items[listbox_play_index].ToString().StartsWith("»") || listBox1.Items[listbox_play_index].ToString().StartsWith("ǁ"))
+					{
+						listBox1.Items[listbox_play_index] = listBox1.Items[listbox_play_index].ToString().Substring(2);
+					}
+					listBox1.Items[listbox_play_index] = "» " + listBox1.Items[listbox_play_index].ToString();
+					break;
+				case 2:		//paused
+					if (listBox1.Items[listbox_play_index].ToString().StartsWith("»") || listBox1.Items[listbox_play_index].ToString().StartsWith("ǁ"))
+					{
+						listBox1.Items[listbox_play_index] = listBox1.Items[listbox_play_index].ToString().Substring(2);
+					}
+					listBox1.Items[listbox_play_index] = "ǁ " + listBox1.Items[listbox_play_index].ToString();
+					break;
+				case 0:		//ended
+					if (listBox1.Items[listbox_play_index].ToString().StartsWith("»") || listBox1.Items[listbox_play_index].ToString().StartsWith("ǁ"))
+					{
+						listBox1.Items[listbox_play_index] = listBox1.Items[listbox_play_index].ToString().Substring(2);
+					}
+
+					if (listBox1.Items[listbox_play_index + 1] != null)
+					{
+						listbox_play_index++;
+						string item = listBox1.Items[listbox_play_index].ToString();
+						url_global = item + "?autoplay=1&version=3&enablejsapi=1&autohide=1";
+						ende = true;
+					}
+					break; 
+				//case 3: ; break; //buffering
+			}
+		}
+		
+		private void YTStateError(string error)
+		{
+			Console.Write("YTplayer_error: " + error + "\r\n");
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ThreadPool.QueueUserWorkItem(play);
+			width = this.Width;
+			height = this.Height;
+
+			listBox1.Items.Clear();
+			listBox1.Items.AddRange(Properties.Settings.Default.liste.Split(';'));
+		}
+
+		private void play(object state)
+		{
+			while(true)
+			{
+				if (url_global.Length > 0 && ende == true)
+				{
+					ende = false;
+					YTplayer.Movie = url_global;
+				}
+				Thread.Sleep(500);
+			}
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listBox1.SelectedIndex >= 0)
+			{
+				button2.Enabled = true;
+			}
+			else
+			{
+				button2.Enabled = false;
+			}
+			//YTplayer.
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int index = listBox1.SelectedIndex;
+			if(listbox_play_index != index)
+			{
+				listBox1.Items.RemoveAt(index);
+			}
+
+			if (listbox_play_index > index)
+			{
+				listbox_play_index--;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			string temp = "";
+			foreach(string t in listBox1.Items)
+			{
+				if(temp.Length != 0)
+				{
+					temp += ";";
+				}
+
+				if (t.StartsWith("»") || t.StartsWith("ǁ"))
+				{
+					temp += t.Substring(2);
+				}
+				else
+				{
+					temp += t;
+				}
+			}
+
+			Properties.Settings.Default.liste = temp;
+			Properties.Settings.Default.Save();
+		}
+
+		private void YTplayer_ControlAdded(object sender, ControlEventArgs e)
+		{
+			MessageBox.Show("I");
+		}
+	}
+}
